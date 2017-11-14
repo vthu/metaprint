fa153ea3861abc04c7c7d43b3cb5ffdf3a87fc74
Vishal Thukral <vthukral@paypal.com>
2017-11-14 16:13:32 +0100

    Plugin Bootstraped

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: ['babel-polyfill', path.resolve(__dirname, 'public/App.js')],
+    output: {
+        path: path.resolve(__dirname, '.build/js'),
+        filename: 'components.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
+
+//Optimize for Production
+config.plugins = config.plugins || [];
+
+//Use Production React
+// config.plugins.push(new webpack.DefinePlugin({
+//   process: {
+//     env: {
+//       NODE_ENV: JSON.stringify("production")
+//     }
+//   }
+// }));
+//Minify The Bundle
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  compress: { warnings: false }
+}));
+
+module.exports = config;
